6a587330aafe4429d2e10bc33c18dc6e44896299
Ronak <32793372+ronakonline@users.noreply.github.com>
2021-07-16 21:23:38 +0530

    language data

--- README.md
+++ README.md
@@ -24,7 +24,7 @@
 ---
 ### <img src='https://media1.giphy.com/media/du3J3cXyzhj75IOgvA/giphy.gif?cid=ecf05e47x2g034i9pzwtzzsd3xgg2w9nr94t4tflbbgo3008&rid=giphy.gif' width='25px'> My Github Stats:
 ![Ronak's github stats](https://github-readme-stats.vercel.app/api?username=ronakonline&show_icons=true&title_color=ffc857&icon_color=8ac926&text_color=daf7dc&bg_color=151515&hide=["stars"])
-[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=ronakonline&layout=compact&text_color=daf7dc&bg_color=151515)](https://github.com/ronakonline/github-readme-stats)
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=ronakonline&layout=compact&text_color=daf7dc&bg_color=151515&hide=html)](https://github.com/ronakonline/github-readme-stats)
 
 <!--START_SECTION:waka-->
 **I'm a Night 🦉** 
--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const readme = `\
 
 ---
 ### <img src='https://media1.giphy.com/media/du3J3cXyzhj75IOgvA/giphy.gif?cid=ecf05e47x2g034i9pzwtzzsd3xgg2w9nr94t4tflbbgo3008&rid=giphy.gif' width='25px'> My Github Stats:
-![Apoorv's github stats](https://github-readme-stats.vercel.app/api?username=ronakonline&show_icons=true&title_color=ffc857&icon_color=8ac926&text_color=daf7dc&bg_color=151515&hide=["stars"])
+![Ronak's github stats](https://github-readme-stats.vercel.app/api?username=ronakonline&show_icons=true&title_color=ffc857&icon_color=8ac926&text_color=daf7dc&bg_color=151515&hide=["stars"])
 [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=ronakonline&layout=compact&text_color=daf7dc&bg_color=151515&hide=html)](https://github.com/anuraghazra/github-readme-stats)
 
 <!--START_SECTION:waka-->
